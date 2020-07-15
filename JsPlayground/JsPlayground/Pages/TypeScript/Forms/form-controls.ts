fe3627905431946442f427f5d302afe4b0a94467
RedBirdOBX <shane.fowlkes.70@gmail.com>
2020-07-15 08:52:51 -0400

    null corrections

--- JsPlayground/JsPlayground/Pages/TypeScript/Forms/form-controls.ts
+++ JsPlayground/JsPlayground/Pages/TypeScript/Forms/form-controls.ts
@@ -5,18 +5,18 @@ namespace TypeScriptDemos.Forms
         static GetFormValues(): void
         {
             let output: string = "";
-            let demo: HTMLElement | null = <HTMLElement>document.getElementById("demo");
+            let demo: HTMLElement = <HTMLElement>document.getElementById("demo");
 
             // drop down
-            let colorsList: HTMLSelectElement | null = <HTMLSelectElement>document.getElementById("ColorList");
+            let colorsList: HTMLSelectElement = <HTMLSelectElement>document.getElementById("ColorList");
             output += `Color value: ${colorsList.value}, Color text: ${colorsList.selectedOptions[0].text}<br />`;
 
             // text box
-            let nameText: HTMLInputElement | null = <HTMLInputElement>document.getElementById("NameText");
+            let nameText: HTMLInputElement = <HTMLInputElement>document.getElementById("NameText");
             output += `Name: ${nameText.value}<br />`;
 
             // checkbox
-            let checkbox1Checkbox: HTMLInputElement | null = <HTMLInputElement>document.getElementById("CheckBox1");
+            let checkbox1Checkbox: HTMLInputElement = <HTMLInputElement>document.getElementById("CheckBox1");
             output += `CheckMe checked?: ${checkbox1Checkbox.checked}<br />`;
 
             // radios
@@ -24,7 +24,7 @@ namespace TypeScriptDemos.Forms
             // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
             // https://stackoverflow.com/questions/9561625/checking-value-of-radio-button-group-via-javascript
 
-            let radioSelection: HTMLInputElement | null = document.querySelector('input[name="RadioGroup1"]:checked');
+            let radioSelection: HTMLInputElement = <HTMLInputElement>document.querySelector('input[name="RadioGroup1"]:checked');
             output += `Selected Radio: ${radioSelection!.value}<br />`;
 
             // results
