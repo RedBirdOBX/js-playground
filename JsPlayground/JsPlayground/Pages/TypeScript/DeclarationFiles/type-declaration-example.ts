01a68bff5c918a4bffadee0af622fca1e79587c7
RedBirdOBX <shane.fowlkes.70@gmail.com>
2020-07-15 08:46:49 -0400

    build correction

--- JsPlayground/JsPlayground/Pages/TypeScript/DeclarationFiles/type-declaration-example.ts
+++ JsPlayground/JsPlayground/Pages/TypeScript/DeclarationFiles/type-declaration-example.ts
@@ -2,7 +2,7 @@ namespace TypeScriptDemos.DeclarationFiles
 {
     window.onload = function ()
     {
-        let msg: string = "Some men just want to watch the world burn.";
+        //let msg: string = "Some men just want to watch the world burn.";
         //$("#demo1").html(msg);
     };
 }
