2fb147dc70264c621d5b8a7326fed6c9c76faf66
RedBirdOBX <shane.fowlkes.70@gmail.com>
2020-07-27 10:36:18 -0400

    Ajax work

---
+++ JsPlayground/JsPlayground/Pages/jquery/ajax/Ajax.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsPlayground.Pages.jquery.ajax
+{
+    public class AjaxModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
