2938dc508c603206ea503b316ec6fb976ec2b767
Shane Fowlkes <shane.fowlkes@cedarfair.com>
2020-09-07 09:16:56 -0400

    added spread

---
+++ JsPlayground/JsPlayground/Pages/Javascript/Arrays/Spread.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsPlayground.Pages.Javascript.Arrays
+{
+    public class SpreadModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
