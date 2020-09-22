394cd3875916431d90c218c465011eb0d8ad337f
RedBirdOBX <shane.fowlkes.70@gmail.com>
2020-09-22 08:21:46 -0400

    array methods update

---
+++ JsPlayground/JsPlayground/wwwroot/assets/javascripts/SampleAuthorsArray.js
@@ -0,0 +1,40 @@
+﻿const authors =
+    [
+        {
+            name: 'Mark Twain',
+            imageUrl: 'Images/Authors/mark-twain.jpg',
+            books: ['The Adventures of Huckleberry Finn', 'Life on the Mississippi']
+        },
+        {
+            name: 'Joseph Conrad',
+            imageUrl: 'Images/Authors/joseph-conrad.jpg',
+            books: ['Heart of Darkness']
+        },
+        {
+            name: 'J.K Rowling',
+            imageUrl: 'Images/Authors/jk-rowling.jpg',
+            books: ['Harry Potter and the Sorcerers Stone']
+        },
+        {
+            name: 'Stephen King',
+            imageUrl: 'Images/Authors/stephen-king.jpg',
+            books: ['The Shining', 'It']
+        },
+        {
+            name: 'William Shakespeare',
+            imageUrl: 'Images/Authors/william-shakespeare.jpg',
+            books: ['Hamlet', 'Macbeth']
+        },
+        {
+            name: 'Arthur C Clarke',
+            imageUrl: 'Images/Authors/arthur-c-clarke.jpg',
+            imageSource: 'Wikimedia Commons',
+            books: ['2001', 'Rendevous with Rama']
+        },
+        {
+            name: 'Isaas Asimov',
+            imageUrl: 'Images/Authors/isaac-asimov.jpg',
+            imageSource: 'Wikimedia Commons',
+            books: ['The Gods Themselves', 'Robot Dreams', 'The Naked Sun']
+        }
+    ];
