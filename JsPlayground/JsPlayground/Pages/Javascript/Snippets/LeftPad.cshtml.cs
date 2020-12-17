21165c12ea8504a77343e35860463149e9a36f1d
RedBirdOBX <shane.fowlkes.70@gmail.com>
2020-12-17 08:20:07 -0500

    adding left pad

---
+++ JsPlayground/JsPlayground/Pages/Javascript/Snippets/LeftPad.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsPlayground.Pages.Javascript.Snippets
+{
+    public class LeftPadModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
