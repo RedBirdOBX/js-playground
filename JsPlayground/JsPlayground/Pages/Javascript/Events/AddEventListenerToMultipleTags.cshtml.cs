508ef290028e0511581afd86accd1d8f0dd9c69e
Shane Fowlkes <shane.fowlkes@cedarfair.com>
2021-03-02 11:13:16 -0500

    event additions

---
+++ JsPlayground/JsPlayground/Pages/Javascript/Events/AddEventListenerToMultipleTags.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsPlayground.Pages.Javascript.Events
+{
+    public class AddEventListenerToMultipleTagsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ JsPlayground/JsPlayground/Pages/jquery/events/DetectingEnterKey.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsPlayground.Pages.jquery.events
+{
+    public class DetectingEnterKeyModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
